81c0f360ca69bc14d57dab1805edaa6b12a37442
Sai Sashreek D <saisashreekd@gmail.com>
2025-05-27 13:00:50 +0530

    Added Custom Fonts

---
+++ App.jsx
@@ -0,0 +1,50 @@
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { StatusBar } from "expo-status-bar";
+import { useCallback } from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+SplashScreen.preventAutoHideAsync();
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    "Montserrat-Regular": require("./assets/fonts/Montserrat-Regular.ttf"),
+    "Montserrat-Medium": require("./assets/fonts/Montserrat-Medium.ttf"),
+    "Montserrat-SemiBold": require("./assets/fonts/Montserrat-SemiBold.ttf"),
+    "Montserrat-Bold": require("./assets/fonts/Montserrat-Bold.ttf"),
+    "Quicksand-Regular": require("./assets/fonts/Quicksand-Regular.ttf"),
+    "Quicksand-Medium": require("./assets/fonts/Quicksand-Medium.ttf"),
+    "Quicksand-SemiBold": require("./assets/fonts/Quicksand-SemiBold.ttf"),
+    "Quicksand-Bold": require("./assets/fonts/Quicksand-Bold.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) return null;
+
+  return (
+    <View style={styles.container} onLayout={onLayoutRootView}>
+      <Text style={styles.containerText}>App</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#E8F9FF",
+    width: "100%",
+    height: "100%",
+  },
+  containerText: {
+    margin: 40,
+    textAlign: "center",
+    fontSize: 20,
+    fontFamily: "Montserrat-Medium"
+  },
+});
